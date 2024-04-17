658fca30d62ffef99ad5475b4d5efd3b86456f68
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-04-17 16:20:58 +0200

    add index characters

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// import MyComp from './components/MyComp.vue';
+// import {store} from './store/index.js'
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      characters: [],
+    };
+  },
+
+  props: {
+    // ...
+  },
+
+  methods: {
+    fetchCharacters() {
+      axios.get("http://127.0.0.1:8000/api/characters").then((res) => {
+        this.characters = res.data.characters.data;
+      });
+    },
+  },
+
+  components: {},
+
+  created() {
+    this.fetchCharacters();
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1 class="my-5">Play</h1>
+    <div class="row row-cols-4 g-3">
+      <div v-for="character in characters" class="col">
+        <div class="card">
+          <img :src="character.type.image" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{{ character.name }}</h5>
+            <ul class="list-group">
+              <li class="list-group-item">Forza {{ character.strength }}</li>
+              <li class="list-group-item">Difesa {{ character.defence }}</li>
+              <li class="list-group-item">Velocità {{ character.speed }}</li>
+              <li class="list-group-item">Vita {{ character.life }}</li>
+              <li class="list-group-item">Intelligenza {{ character.intelligence }}</li>
+            </ul>
+            <a href="#" class="btn btn-primary">Scegli me!</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use "./general.scss";
 </style>
